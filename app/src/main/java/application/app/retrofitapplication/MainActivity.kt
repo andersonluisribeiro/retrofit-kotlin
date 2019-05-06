f8e2981b55e148645c0b0eab7beddb3b60adcc18
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-05-06 20:06:38 -0300

    removing code

--- app/src/main/java/application/app/retrofitapplication/MainActivity.kt
+++ app/src/main/java/application/app/retrofitapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package application.app.retrofitapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
